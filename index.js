11a03aa45a0ba898e3b98d1d60f9f0d79a9b7533
franciscocid <franciscocidadehomemjunior@gmail.com>
2020-12-16 18:38:59 -0300

    feat: searchTerm on prefix question

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const readline = require('readline-sync')
 function start() {
   const content = {}
   content.searchTerm = askAndReturnSearchTerm()
-  content.prefix = askAndReturnPrefix()
+  content.prefix = askAndReturnPrefix(content.searchTerm)
 
   function askAndReturnSearchTerm() {
     return readline.question('Type a Wikipedia search term: ')
   }
 
-  function askAndReturnPrefix() {
+  function askAndReturnPrefix(searchTerm) {
     const prefixes = ['Who is', 'What is', 'The history of']
-    const selectedPrefixIndex = readline.keyInSelect(prefixes)
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, `Select a prefix for ${searchTerm}:`)
     return prefixes[selectedPrefixIndex]
   }
 
