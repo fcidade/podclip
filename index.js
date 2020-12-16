e90e12018d0aa66be5c1fb17b9d955ce224acef0
franciscocid <franciscocidadehomemjunior@gmail.com>
2020-12-16 18:03:21 -0300

    feat: select prefix

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const readline = require('readline-sync')
 function start() {
   const content = {}
   content.searchTerm = askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
 
   function askAndReturnSearchTerm() {
-    return readline.question('Type a Wikipedia searcg term: ')
+    return readline.question('Type a Wikipedia search term: ')
+  }
+
+  function askAndReturnPrefix() {
+    const prefixes = ['Who is', 'What is', 'The history of']
+    const selectedPrefixIndex = readline.keyInSelect(prefixes)
+    return prefixes[selectedPrefixIndex]
   }
 
   console.log(content)
