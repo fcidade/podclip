7bb0e47dd9802d2f4fcb863a2302cd1251288032
franciscocid <franciscocidadehomemjunior@gmail.com>
2020-12-20 18:05:01 -0300

    feat: cutter robot generate small clip from original video

--- podclip/robots/cutter_robot.py
+++ podclip/robots/cutter_robot.py
@@ -1,13 +1,18 @@
 from podclip.robot import Robot, RobotState
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import logging
 
 class CutterRobot(Robot):
   
   def run(self, state: RobotState) -> RobotState:
     logging.info(f'Generating clip from "{state.video_path}"')
-    logging.info(f'Start: {state.clip_start} End: {state.clip_end}')
+    logging.info(f'Start: {state.clip_start}s End: {state.clip_end}s')
 
-    
+    ffmpeg_extract_subclip(
+      state.video_path, 
+      state.clip_start, state.clip_end, 
+      targetname=state.clip_path
+    )
 
     logging.info(f'Clip generated on "{state.clip_path}"')
     return state
