8980ec7274d71f00ce26f4d23e7277cade2fd31d
franciscocid <franciscocidadehomemjunior@gmail.com>
2020-12-20 18:05:11 -0300

    test: cutter robot

--- test/test_cutter_robot.py
+++ test/test_cutter_robot.py
@@ -1,14 +1,21 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 from podclip.robots import CutterRobot
 from podclip.robot import RobotState
+from os.path import join
+import logging
 
 class TestCutterRobot(TestCase):
 
-  def test_(self):
+  def setUp(self):
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+  def test_subclip_called_with_right_parameters(self):
     prev_state = RobotState(
-      video_path='resources/videos/teste.mp3',
+      video_path=join('test', 'resources', 'videos', 'a deriva 6 - arthur petry.mp4'),
       clip_start=0,
-      clip_end=0,
-      clip_path='resources/videos/clip01.mp4'
+      clip_end=10,
+      clip_path=join('test', 'resources', 'videos', 'clip0.mp4'),
     )
-    state = CutterRobot().run(prev_state)
+    with mock.patch('podclip.robots.cutter_robot.ffmpeg_extract_subclip') as m:
+      state = CutterRobot().run(prev_state)
+      m.assert_called_once_with(prev_state.video_path, 0, 10, targetname=prev_state.clip_path)
