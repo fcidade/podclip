4d3a764915f39901b0c091338a98efb71ac99cb4
franciscocid <franciscocidadehomemjunior@gmail.com>
2020-12-20 19:55:18 -0300

    feat: audio extraction robot

---
+++ podclip/robots/audio_extractor_robot.py
@@ -0,0 +1,12 @@
+from podclip.robot import Robot, RobotState
+import moviepy.editor as mp
+import logging
+
+class AudioExtractorRobot(Robot):
+  
+  def run(self, state: RobotState) -> RobotState:
+    logging.info(f'Converting video "{state.clip_path}" to audio...')
+
+    mp.VideoFileClip(state.clip_path).audio.write_audiofile(state.clip_audio_path)
+
+    return state
